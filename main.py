c3f8bf136b3b341f9c5cb135bdece4806a44e479
Rishabh Kumar <rishabh.kumar@outlook.com>
2023-06-01 15:09:00 +0200

    Third Commit

--- main.py
+++ main.py
@@ -11,21 +11,22 @@ from temperature.hot_pot import HotPotTemperature
 from pressure.main_vessel import MainVesselPressure
 from weight.main_vessel import MainVesselWeight
 from weight.hot_pot import HotPotWeight
-##
 
 
 deviceTypes = {
-    "Anchor":Anchor,
-    "Cowl":Cowl,
-    "Emulsifier":Emulsifier,
-    "Hot Pot Load Cell":HotPotWeight,
-    "Hot Pot Temperature":HotPotTemperature,
-    "Inner Agitator":InnerAgitator,
-    "Main Vessel Load Cell":MainVesselWeight,
-    "Main Vessel Pressure":MainVesselPressure,
-    "Main Vessel Temperature":MainVesselTemperature,
-    "Paddle":Paddle,
+    "Anchor" : Anchor,
+    "Cowl" : Cowl,
+    "Emulsifier" : Emulsifier,
+    "Hot Pot Load Cell" : HotPotWeight,
+    "Hot Pot Temperature" : HotPotTemperature,
+    "Inner Agitator" : InnerAgitator,
+    "Main Vessel Load Cell" : MainVesselWeight,
+    "Main Vessel Pressure" : MainVesselPressure,
+    "Main Vessel Temperature" : MainVesselTemperature,
+    "Paddle" : Paddle,
 }
+
+devices = []
 
 
 def getAllVesselDevices():
@@ -43,10 +44,12 @@ def getAllVesselDevices():
     payloads = json.loads(response.content.decode("utf-8"))["payload"]
     for payload in payloads:
         model = deviceTypes[payload["device_type"]["description"]](payload)
-        model.read()
+        devices.append(model)
+
 
 def main():
     getAllVesselDevices()
+    print(devices)
     pass
 
 
